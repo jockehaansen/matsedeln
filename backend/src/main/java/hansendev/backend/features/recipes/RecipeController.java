d50656975b044d2d72b426545f5cf9304a62fece
Joakim Hansen <jocke.hansen@outlook.com>
2026-02-03 10:33:40 +0100

    fix: api mapping fix and user cred seed

--- backend/src/main/java/hansendev/backend/features/recipes/RecipeController.java
+++ backend/src/main/java/hansendev/backend/features/recipes/RecipeController.java
@@ -5,11 +5,13 @@ import hansendev.backend.features.recipes.models.Recipe;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/api/recipes")
+@RestController
+@RequestMapping("api/recipes/")
 @RequiredArgsConstructor
 public class RecipeController {
 
